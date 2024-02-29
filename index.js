a05f25a0a29239ab8fc6d3f397dba225a09c18a3
keithchasen <keithchasen89@gmail.com>
2024-02-29 20:53:37 +0100

    load map image

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ canvas.height = 576;
 c.fillStyle = 'white';
 
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+ const image = new Image();
+ image.src = './img/pokemonMap.png';
+
+ image.onload = () => {
+    c.drawImage(image, 0, 0);
+ }
