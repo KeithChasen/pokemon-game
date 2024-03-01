67fbd8be833d72f3b5a34c5d83a7341f47c87e69
keithchasen <keithchasen89@gmail.com>
2024-03-01 22:25:02 +0100

    top map image

--- index.js
+++ index.js
@@ -12,30 +12,6 @@ for (let i = 0; i < collisions.length; i+= 70) {
     collisionMap = [...collisionMap, collisions.slice(i, i + 70)];
 }
 
-class Boundary {
-    static width = 48;
-    static height = 48;
-
-    constructor({ position }) {
-        this.position = position;
-
-        // original tiles is 12 pixels
-        // but we exported 400% image
-        this.width = 48;
-        this.height = 48;
-    }
-
-    draw(c) {
-        c.fillStyle = 'rgba(255, 0, 0, 0)';
-        c.fillRect(
-            this.position.x, 
-            this.position.y,
-            this.width,
-            this.height
-        )
-    }
-}
-
 const offset = {
     x: -950, 
     y: -350 
@@ -59,43 +35,20 @@ collisionMap.forEach((row, i) => {
  const image = new Image();
  image.src = './img/pokemonMap.png';
 
+ const foregroundImage = new Image();
+ foregroundImage.src = './img/pokemonMapTop.png';
+
  const playerImage = new Image();
  playerImage.src = './img/playerDown.png';
-
- class Sprite {
-    constructor({
-        position,
-        image,
-        frames = { max: 1 }
-    }) {
-        this.position = position;
-        this.image = image;
-        this.frames = frames;
-
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max
-            this.height = this.image.height
-        }
-    }
-
-    draw(c) {
-        c.drawImage(
-            this.image, 
-            0,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
-        );
-    }
- }
 
  const background = new Sprite({
     position: { x: offset.x, y: offset.y },
     image: image
+ });
+
+ const foreground = new Sprite({
+    position: { x: offset.x, y: offset.y },
+    image: foregroundImage
  });
 
  const player = new Sprite({
@@ -117,7 +70,7 @@ collisionMap.forEach((row, i) => {
     d: { pressed: false },
  }
 
- const movables = [background, ...boundaries];
+ const movables = [background, foreground, ...boundaries];
 
  function rectangularCollision(rect1, rect2) {
     return rect1.position.x + rect1.width >= rect2.position.x &&
@@ -136,6 +89,8 @@ collisionMap.forEach((row, i) => {
     })
 
     player.draw(c);
+
+    foreground.draw(c);
 
     if (keys.w.pressed) {
         let moving = true;
